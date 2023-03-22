03ca0e5a2db1faec82ab2c780b744aef8bc5a873
TrakaDev <trakadev@gmail.com>
2023-03-22 00:51:50 -0600

    update social media links

--- src/data/socialMedia.js
+++ src/data/socialMedia.js
@@ -1,9 +1,9 @@
 export const SocialMediaLinks = {
-    discord: "#",
-    youtube: "#",
-    facebook: "#",
-    twitter: "#",
-    twitch: "#",
-    instagram: "#",
-    tiktok: "#",
+    discord: "https://discord.gg/HQunfWtKR9",
+    youtube: "https://www.youtube.com/@ELTEMACH",
+    facebook: "https://www.facebook.com/eltemach",
+    twitter: "https://twitter.com/el_temach",
+    twitch: "https://www.twitch.tv/eltemach",
+    instagram: "https://www.instagram.com/eltemach",
+    tiktok: "https://www.tiktok.com/@el_temach",
 }
